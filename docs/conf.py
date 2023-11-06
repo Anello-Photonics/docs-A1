7a3351ed421d9cab6b09c201a4c598c80dc85cb7
Kirstin Schauble <kirstin.schauble@anellophotonics.com>
2023-11-05 16:20:38 -0800

    Debug duplicate section titles

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,8 @@ needs_sphinx = '1.5.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.mathjax',
-              'sphinx.ext.autosectionlabel',
               'sphinx.ext.ifconfig',
-              'recommonmark']
+              'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
